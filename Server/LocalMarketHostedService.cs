c1bbd3865ec53ef2f49fbdbf28ed0451d2ade1cb
Maks <akimtsev.maks@gmail.com>
2026-08-07 23:28:29 +0700

    feat: add LocalMarket hosted lifecycle

---
+++ Server/LocalMarketHostedService.cs
@@ -0,0 +1,21 @@
+using exchange_simulator.Services;
+
+namespace exchange_simulator.Server;
+
+internal sealed class LocalMarketHostedService(LocaпlMarket market): IHostedService
+{
+    private readonly LocalMarket _market = market;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        cancellationToken.ThrowIfCancellationRequested();
+        
+        return _market.StartAsync(cancellationToken);
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken)
+    {
+        return _market.StopAsync().WaitAsync(cancellationToken);
+    }
+
+}
---
+++ Server/Program.cs
@@ -0,0 +1,37 @@
+using exchange_simulator.Bots;
+using exchange_simulator.Models.TradingCore;
+using exchange_simulator.Server;
+using exchange_simulator.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddSingleton(CreateMarket());
+builder.Services.AddHostedService<LocalMarketHostedService>();
+
+var app = builder.Build();
+
+app.Run();
+
+static LocalMarket CreateMarket()
+{
+    var instrument = new Instrument(
+        Guid.NewGuid(),
+        "TEST",
+        "Test Instrument",
+        lotSize: 1,
+        initialPrice: 100m);
+
+    return new LocalMarket(
+        instrument,
+        initialCashPerAccount: 100000m,
+        initialInstrumentsPerAccount: 1000,
+        stepInterval: TimeSpan.FromSeconds(1),
+        marketMakerOptions: new MarketMakerBotOptions(
+            QuoteOffset: 1m,
+            OrderSize: 10),
+        noiseBotOptions: new NoiseBotOptions(
+            RandomSeed: 111,
+            PriceOffset: 3m,
+            MaxOrderLots: 5,
+            MaxActiveOrders: 20));
+}
