c5cdc33d811e98b15880cd1bb3fd6ea73ad716f0
Maks <akimtsev.maks@gmail.com>
2026-08-19 17:00:04 +0700

    feat: configure instrument storage

--- exchange-simulator.Server/Persistence/ExchangeDbContext.cs
+++ exchange-simulator.Server/Persistence/ExchangeDbContext.cs
@@ -39,6 +39,7 @@ internal sealed class ExchangeDbContext(
     {
         base.OnModelCreating(modelBuilder);
         ConfigureTradingWorld(modelBuilder);
+        ConfigureInstrument(modelBuilder);
     }
 
     private static void ConfigureTradingWorld(ModelBuilder modelBuilder)
@@ -63,5 +64,53 @@ internal sealed class ExchangeDbContext(
 
         entity.Property(world => world.InitialCashPerAccount)
             .HasColumnType("numeric");
+    }
+    
+    private static void ConfigureInstrument(ModelBuilder modelBuilder)
+    {
+        var entity = modelBuilder.Entity<InstrumentEntity>();
+
+        entity.ToTable("Instruments", table =>
+        {
+            table.HasCheckConstraint(
+                "CK_Instruments_Ticker_Length",
+                "char_length(\"Ticker\") = 4");
+
+            table.HasCheckConstraint(
+                "CK_Instruments_Name_Length",
+                "char_length(\"Name\") BETWEEN 1 AND 99");
+
+            table.HasCheckConstraint(
+                "CK_Instruments_LotSize_Positive",
+                "\"LotSize\" > 0");
+
+            table.HasCheckConstraint(
+                "CK_Instruments_InitialPrice_Positive",
+                "\"InitialPrice\" > 0");
+        });
+
+        entity.HasKey(instrument => instrument.Id);
+
+        entity.Property(instrument => instrument.Id)
+            .ValueGeneratedNever();
+
+        entity.Property(instrument => instrument.Ticker)
+            .HasMaxLength(4)
+            .IsRequired();
+
+        entity.Property(instrument => instrument.Name)
+            .HasMaxLength(99)
+            .IsRequired();
+
+        entity.Property(instrument => instrument.InitialPrice)
+            .HasColumnType("numeric");
+
+        entity.HasIndex(instrument => instrument.WorldId)
+            .IsUnique();
+
+        entity.HasOne<TradingWorldEntity>()
+            .WithMany()
+            .HasForeignKey(instrument => instrument.WorldId)
+            .OnDelete(DeleteBehavior.Restrict);
     }
 }
